da4649461066145c7b1cc91fe4e3b2e6b7537ece
Kelvin Tran <kelvin.tran1234+github@gmail.com>
2023-08-05 18:52:10 -0500

    Add interface/link objects

--- device/device.py
+++
@@ -1,8 +0,0 @@
-from . import deviceobject
-
-def buildSupportedDevices(allDevices, platform):
-    all = [deviceobject.Device(labDevice, platform)
-                  for labDevice in allDevices]
-    supported = [device for device in all if device.os is not None]
-
-    return {"all": all, "supported": supported}
--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from urllib3 import disable_warnings
 disable_warnings()
 
 from api import apicaller
-from device import device
+from lab import device
 from settings.readSettings import readSettings
 
 if __name__ == "__main__":
     settings = readSettings()
     api = apicaller.createAuthedAPISession(settings["general"]["url"], settings["general"]["trustcert"])
     devices = device.buildSupportedDevices(api.buildDevicesList(settings["platform"]["lab_id"]), settings["platform"]["appliance"])
-
-    print(*devices["all"], sep="\n")

990384a93ed4ee3177b0829e4c9e96137fc9c8b6
Kelvin Tran <kelvin.tran1234+github@gmail.com>
2023-08-05 06:58:28 -0500

    Add settings file

--- api/apicaller.py
+++ api/apicaller.py
@@ -1,10 +1,11 @@
 from . import cmlapi, gns3api, eveapi
 from getpass import getpass
 
-def createAuthedAPISession():
-    apiObject = cmlapi.API(baseURL="https://ktl-csco-cml01.lab.trankelvin.com/api/v0",
+def createAuthedAPISession(url, verify):
+    apiObject = cmlapi.API(baseURL=f"https://{url}/api/v0",
                             username=input("Enter your username: "),
-                            password=getpass("Enter your password: "))
+                            password=getpass("Enter your password: "),
+                            verifyStatus=verify)
 
     if apiObject.authAPI() is not None:
         print("Authentication error. Validate your credentials/parameters and try again.")
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ disable_warnings()
 
 from api import apicaller
 from device import device
+from settings.readSettings import readSettings
 
 if __name__ == "__main__":
-    api = apicaller.createAuthedAPISession()
-    devices = device.buildSupportedDevices(api.buildDevicesList(), "cml")
+    settings = readSettings()
+    api = apicaller.createAuthedAPISession(settings["url"], settings["trustcert"])
+    devices = device.buildSupportedDevices(api.buildDevicesList(), settings["platform"])

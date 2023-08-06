aa8cb0e735db9d5d3759d758a296033401dab57a
Kelvin Tran <kelvin.tran1234+github@gmail.com>
2023-08-06 14:15:15 -0500

    Change folder structure to allow modular feature addition

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from lab import device
 from settings.readSettings import readSettings
 
 def main():
+    # Configure app settings
     settings = readSettings()
+
+    # Create API session
     api = apicaller.createAuthedAPISession(settings["general"]["url"], settings["general"]["trustcert"])
+    # Gather data about lab objects (devices, links, interfaces, etc.) from API
     allDevices = api.buildDevicesList(settings["platform"]["lab_id"])
     links = api.buildLinksList(settings["platform"]["lab_id"])
     interfaces = [api.buildInterfacesList(settings["platform"]["lab_id"], device["id"])
                   for device in allDevices]
     
+    # Create the device objects for the lab
     devices = device.buildSupportedDevices(allDevices, interfaces, links,
                                            settings["platform"]["appliance"])
 
